387265d02abf126b0af8b5cc5399e6cee7ff7765
Kakudo <kakudo@MBP2017.local>
2018-03-10 23:06:42 +0900

    dev

--- models/sequelizeLoader.js
+++ models/sequelizeLoader.js
@@ -1,14 +1,9 @@
 'use strict';
 const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(
-//   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/n_coin',
-//   { logging: !(process.env.NODE_ENV === 'production') }
-// );
-
-const sequelize = new Sequelize("データベース名", "アクセスユーザー名", "ユーザーパスワード", {
-  host: "localhost",
-  dialect: "postgres"
-})
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/n_coin',
+  { logging: !(process.env.NODE_ENV === 'production') }
+);
 
 module.exports = {
   database: sequelize,
