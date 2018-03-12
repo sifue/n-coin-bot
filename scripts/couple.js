699769a3463b89bf891a68a7390cc2edd6947718
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2018-03-12 21:46:28 +0900

    FIX: 相性占いを1メッセージに

--- scripts/couple.js
+++ scripts/couple.js
@@ -61,61 +61,69 @@ module.exports = robot => {
           10 - Math.abs(targetChemistoryPoint - myChemistoryPoint);
         const persentage = coupleChemistory * 10;
         const firstMessage = `<@${myId}>さんと<@${targetId}>さんとの今日の相性は *${persentage}%* です。\n`;
+        const afterMessage = `\n今日の相性占いのお代として *${priceCoupleCommand}N* コイン頂きます。`;
 
         if (coupleChemistory === 1) {
           msg.send(
             firstMessage +
-              '今日は相手のことを思いやったコミュニケーションを心がけるようにしましょう。'
+              '今日は相手のことを思いやったコミュニケーションを心がけるようにしましょう。' +
+              afterMessage
           );
         } else if (coupleChemistory === 2) {
           msg.send(
             firstMessage +
-              '今日は相手のやさしさや頑張りをもっとねぎらってあげるのが良いでしょう。'
+              '今日は相手のやさしさや頑張りをもっとねぎらってあげるのが良いでしょう。' +
+              afterMessage
           );
         } else if (coupleChemistory === 3) {
           msg.send(
             firstMessage +
-              '今日は相手が何か言いたいことがあるのかもしれません。相手の話をしっかり聞いてあげましょう。'
+              '今日は相手が何か言いたいことがあるのかもしれません。相手の話をしっかり聞いてあげましょう。' +
+              afterMessage
           );
         } else if (coupleChemistory === 4) {
           msg.send(
             firstMessage +
-              '今日は関係性が変わるできごとがあるかもしれません。チャンスを逃さないよう相手をよくみていると良いでしょう。'
+              '今日は関係性が変わるできごとがあるかもしれません。チャンスを逃さないよう相手をよくみていると良いでしょう。' +
+              afterMessage
           );
         } else if (coupleChemistory === 5) {
           msg.send(
             firstMessage +
-              '今日は相手との相性が良くなり始めているタイミングです。積極的にコミュニケーションをとると良いでしょう。'
+              '今日は相手との相性が良くなり始めているタイミングです。積極的にコミュニケーションをとると良いでしょう。' +
+              afterMessage
           );
         } else if (coupleChemistory === 6) {
           msg.send(
             firstMessage +
-              '今日は相性が良いです。二人で何かをするときっと良い方向にことが動くでしょう。'
+              '今日は相性が良いです。二人で何かをするときっと良い方向にことが動くでしょう。' +
+              afterMessage
           );
         } else if (coupleChemistory === 7) {
           msg.send(
             firstMessage +
-              '今日は相性がそこそこ良い日です。相手がナーバスになっているときには良いがげましができるでしょう。'
+              '今日は相性がそこそこ良い日です。相手がナーバスになっているときには良いがげましができるでしょう。' +
+              afterMessage
           );
         } else if (coupleChemistory === 8) {
           msg.send(
             firstMessage +
-              '今日は相性のバランスが良い日です。自分から何かを打ち明けるには良い日になるでしょう。'
+              '今日は相性のバランスが良い日です。自分から何かを打ち明けるには良い日になるでしょう。' +
+              afterMessage
           );
         } else if (coupleChemistory === 9) {
           msg.send(
             firstMessage +
-              '今日は相性がかなり良いです。何か二人で面白いことを企画するには最高の日でしょう。'
+              '今日は相性がかなり良いです。何か二人で面白いことを企画するには最高の日でしょう。' +
+              afterMessage
           );
         } else {
           msg.send(
             firstMessage +
-              '今日は相性が絶好調です。相手を信頼して何かを一緒にすると良いでしょう。'
+              '今日は相性が絶好調です。相手を信頼して何かを一緒にすると良いでしょう。' +
+              afterMessage
           );
         }
-        msg.send(
-          `今日の相性占いのお代として *${priceCoupleCommand}N* コイン頂きます。`
-        );
         sendCoin(robot, msg, me, robot.adapter.self.id, priceCoupleCommand);
       });
     }
