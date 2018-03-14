73c86ffdc54e49be350d3231b8077f090e863b0a
yudai-uehara <yuudai_17n1100760@nnn.ed.jp>
2018-03-15 02:55:57 +0900

    Feat: auctionfinishコマンドを追加

--- scripts/auction.js
+++ scripts/auction.js
@@ -18,7 +18,8 @@ Deal.sync();
 AuctionItem.sync();
 AuctionBid.sync();
 const maxPrice = 100000;
-const auctionChannelId = 'C9QL4HQLW';
+// const auctionChannelId = 'C9QL4HQLW';
+const auctionChannelId = 'C84P7122E';
 const auctionItemMsgFormat =
   '[<id>] *<description>*\n出品者: <@<userId>>\n出品時刻: *<date>*\n入札受付期間: *残り<timeLimit>日*\nスタート価格: *<startPrice>N*';
 const auctionBidLogMsgFormat =
@@ -26,99 +27,46 @@ const auctionBidLogMsgFormat =
 
 module.exports = robot => {
   //毎時に商品の残り時間を更新
-  cron.schedule(`0 0 * * * *`, () => {
+  cron.schedule(`0 39 2 * * *`, () => {
     AuctionItem.findAll().then(auctionItems => {
       if (auctionItems.length) {
         auctionItems.forEach(auctionItem => {
           const createdAt = auctionItem.dataValues.createdAt;
-          if (moment(createdAt).hours() === moment().hour()) {
-            const timeLimit = auctionItem.dataValues.timeLimit;
-            const auctionitemId = auctionItem.dataValues.id;
-            const description = auctionItem.dataValues.description;
-            const userId = auctionItem.dataValues.userId;
-            //入札受付期間満了
-            if (timeLimit <= 1) {
-              AuctionBid.findAll({
-                where: { auctionitemId: auctionitemId },
-                order: [['bidAmount', 'DESC']]
-              })
-                .then(auctionBids => {
-                  if (auctionBids.length) {
-                    //入札者が居た場合
-                    const finishPrice = auctionBids[0].dataValues.bidAmount;
-                    const auctionWinnerId = auctionBids[0].dataValues.userId;
-                    let bidLog = auctionBids
-                      .map(m => {
-                        const date = m.dataValues.createdAt;
-                        const formatedDate = moment(date).format(dateFormat);
-                        const bidAmount = m.dataValues.bidAmount;
-                        const userId = m.dataValues.userId;
-                        return auctionBidLogMsgFormat
-                          .replace('<date>', formatedDate)
-                          .replace('<userName>', `<@${userId}>`)
-                          .replace('<bidAmount>', bidAmount);
-                      })
-                      .join('\n');
-
-                    //オークションチャンネル
-                    robot.messageRoom(
-                      auctionChannelId,
-                      `[${auctionitemId}] *${description}* を <@${auctionWinnerId}> が *${finishPrice}N* コインで落札しました！:tada:`
-                    );
-
-                    //出品者DM
-                    robot.messageRoom(
-                      userId,
-                      `オークションに出品していた [${auctionitemId}] *${description}* の入札期限日になりました。\n結果: <@${auctionWinnerId}> が *${finishPrice}N* コインで落札しました！\n落札者と連絡を取りましょう、落札者と連絡が取れない場合は、入札履歴を確認して落札者を繰り上げましょう。\n入札履歴:\n${bidLog}`
-                    );
-
-                    //落札者DM
-                    robot.messageRoom(
-                      auctionWinnerId,
-                      `[${auctionitemId}] *${description}* をあなたが *${finishPrice}N* コインで落札しました！:tada:\n出品者と連絡を取りましょう。`
-                    );
-                  } else {
-                    //入札者無し 出品者DM
-                    robot.messageRoom(
-                      userId,
-                      `オークションに出品していた [${auctionitemId}] *${description}* の入札期限日になりましたが、入札者は0人でした...:cry:。またのご利用をお待ちしております。`
-                    );
-                  }
-                  return AuctionItem.destroy({ where: { id: auctionitemId } });
-                })
-                .then(auctionItem => {
-                  //オークションアイテム削除時
-                })
-                .catch(e => {
-                  robot.logger.error(e);
-                });
-            } else {
-              AuctionItem.update(
-                {
-                  timeLimit: timeLimit - 1
-                },
-                {
-                  where: { id: auctionitemId }
-                }
-              ).catch(e => {
-                robot.logger.error(e);
-              });
-            }
+          // if (moment(createdAt).hours() === moment().hour()) {
+          const timeLimit = auctionItem.dataValues.timeLimit;
+          const auctionitemId = auctionItem.dataValues.auctionitemId;
+          const description = auctionItem.dataValues.description;
+          const userId = auctionItem.dataValues.userId;
+          //入札受付期間満了
+          if (timeLimit <= 1) {
+            auctionFinish(robot, auctionitemId);
+          } else {
+            AuctionItem.update(
+              {
+                timeLimit: timeLimit - 1
+              },
+              {
+                where: { id: auctionitemId }
+              }
+            ).catch(e => {
+              robot.logger.error(e);
+            });
           }
+          // }
         });
       }
     });
   });
 
   //オークション機能のヘルプ
-  robot.hear(/!nc auction help/i, msg => {
+  robot.hear(/!nc auctionhelp/i, msg => {
     msg.send(
       '■ オークションコマンド一覧\n' +
         '`!nc auction` 出品状況の表示 (出品者に対してメンションが飛ぶためDMでの利用推奨)\n' +
         '`!nc auction {オークションID}` 対象商品の入札状況を確認 (DM推奨)\n' +
-        '`!nc auction add {商品説明} {スタート価格(1以上)} {入札受付期間(日単位/30日以下)}` 出品\n' +
-        '`!nc auction bid {オークションID} {入札価格}` 入札\n' +
-        '`!nc auction delete {オークションID}` 商品の削除'
+        '`!nc auctionadd {商品説明} {スタート価格(1以上)} {入札受付期間(日単位/30日以下)}` 出品\n' +
+        '`!nc auctionbid {オークションID} {入札価格}` 入札\n' +
+        '`!nc auctiondelete {オークションID}` 商品の削除'
     );
   });
 
@@ -134,7 +82,7 @@ module.exports = robot => {
       const auctionitemId = parseInt(parsed[1]);
       let message = '';
       AuctionItem.find({
-        where: { id: auctionitemId }
+        where: { auctionitemId: auctionitemId }
       })
         .then(auctionItem => {
           if (auctionItem) {
@@ -144,7 +92,7 @@ module.exports = robot => {
                 .replace('<userId>', auctionItem.dataValues.userId)
                 .replace('<startPrice>', auctionItem.dataValues.startPrice)
                 .replace('<timeLimit>', auctionItem.dataValues.timeLimit)
-                .replace('<id>', auctionItem.dataValues.id)
+                .replace('<id>', auctionItem.dataValues.auctionitemId)
                 .replace(
                   '<date>',
                   moment(auctionItem.dataValues.createdAt).format(dateFormat)
@@ -220,7 +168,7 @@ module.exports = robot => {
                   .replace('<userId>', auctionItem.dataValues.userId)
                   .replace('<startPrice>', auctionItem.dataValues.startPrice)
                   .replace('<timeLimit>', auctionItem.dataValues.timeLimit)
-                  .replace('<id>', auctionItem.dataValues.id)
+                  .replace('<id>', auctionItem.dataValues.auctionitemId)
                   .replace(
                     '<date>',
                     moment(auctionItem.dataValues.createdAt).format(dateFormat)
@@ -239,13 +187,13 @@ module.exports = robot => {
   });
 
   // オークション出品コマンド
-  robot.hear(/!nc auction add/i, msg => {
+  robot.hear(/!nc auctionadd/i, msg => {
     const parsed = msg.message.rawText.match(
-      /^!nc auction add (.+) (\d+) (\d+)$/
+      /^!nc auctionadd (.+) (\d+) (\d+)$/
     );
     if (!parsed) {
       msg.send(
-        'オークション出品コマンドの形式が `!nc auction add {商品説明} {初期価格(1以上)} {入札受付期間(30日以下)}` ではありません。'
+        'オークション出品コマンドの形式が `!nc auctionadd {商品説明} {初期価格(1以上)} {入札受付期間(30日以下)}` ではありません。'
       );
     } else {
       const userId = msg.message.user.id;
@@ -257,11 +205,11 @@ module.exports = robot => {
   });
 
   // 入札
-  robot.hear(/!nc auction bid/i, msg => {
-    const parsed = msg.message.rawText.match(/^!nc auction bid (\d+) (\d+)$/);
+  robot.hear(/!nc auctionbid/i, msg => {
+    const parsed = msg.message.rawText.match(/^!nc auctionbid (\d+) (\d+)$/);
     if (!parsed) {
       msg.send(
-        '入札コマンドの形式が `!nc auction bid {オークションID} {入札額}` ではありません。'
+        '入札コマンドの形式が `!nc auctionbid {オークションID} {入札額}` ではありません。'
       );
       return;
     }
@@ -272,29 +220,121 @@ module.exports = robot => {
   });
 
   //オークション商品削除コマンド
-  robot.hear(/!nc auction delete/i, msg => {
-    const parsed = msg.message.rawText.match(/^!nc auction delete (\d+)$/);
+  robot.hear(/!nc auctionfinish/i, msg => {
+    const parsed = msg.message.rawText.match(/^!nc auctionfinish (\d+)$/);
     if (!parsed) {
       msg.send(
-        `オークション商品削除コマンドの形式が``!nc auction delete {オークションID} ではありません。```
+        'オークション終了コマンドの形式が`!nc auctionfinish {オークションID}` ではありません。'
       );
     }
-    const userId = msg.message.user.id;
-    const id = parseInt(parsed[1]);
-    AuctionItem.destroy({
-      where: {
-        id: id,
-        userId: userId
-      }
-    }).then(auctionItem => {
-      if (auctionItem) {
-        msg.send(`オークションID [ *${id}* ] の商品を削除しました。`);
-      }
-    });
+    const fromUserId = msg.message.user.id;
+    const auctionitemId = parseInt(parsed[1]);
+    auctionFinish(robot, auctionitemId, fromUserId);
   });
 };
 
-//出品
+/**
+ * オークションを終了
+ * 最後に入札した方を落札者とし、オークションを終了します。
+ * @param {robot} robot apiのrobotオブジェクト
+ * @param {int} auctionitemId 対象商品のID
+ * @param {string} 関数実行者のユーザーID、システム側で定期的に実行するタイミングがある為、この引数は可変長です。
+ */
+function auctionFinish(robot, auctionitemId) {
+  let description;
+  let userId;
+  let fromUserId = arguments[2]; //可変長の引数
+  AuctionItem.find({
+    where: { auctionitemId: auctionitemId }
+  })
+    .then(auctionItem => {
+      if (!auctionItem) {
+        throw 'auction item not found';
+      }
+      description = auctionItem.dataValues.description;
+      userId = auctionItem.dataValues.userId;
+      console.log('fromuserid: ', fromUserId);
+      console.log('userid: ', userId);
+      if (fromUserId && fromUserId != userId) {
+        throw 'permission denied';
+      }
+      return AuctionBid.findAll({
+        where: { auctionitemId: auctionitemId },
+        order: [['bidAmount', 'DESC']]
+      });
+    })
+    .then(auctionBids => {
+      if (auctionBids.length) {
+        //入札者が居た場合
+        const finishPrice = auctionBids[0].dataValues.bidAmount;
+        const auctionWinnerId = auctionBids[0].dataValues.userId;
+        let bidLog = auctionBids
+          .map(m => {
+            const date = m.dataValues.createdAt;
+            const formatedDate = moment(date).format(dateFormat);
+            const bidAmount = m.dataValues.bidAmount;
+            const userId = m.dataValues.userId;
+            return auctionBidLogMsgFormat
+              .replace('<date>', formatedDate)
+              .replace('<userName>', `<@${userId}>`)
+              .replace('<bidAmount>', bidAmount);
+          })
+          .join('\n');
+
+        //オークションチャンネル
+        robot.messageRoom(
+          auctionChannelId,
+          `[${auctionitemId}] *${description}* を <@${auctionWinnerId}> が *${finishPrice}N* コインで落札しました！:tada:`
+        );
+        //出品者DM
+        robot.messageRoom(
+          userId,
+          `あなたがオークションに出品していた *${description}* を <@${auctionWinnerId}> が *${finishPrice}N* コインで落札しました！\n落札者と連絡を取りましょう、連絡が取れない場合は、入札履歴を確認して落札者を繰り上げましょう。\n入札履歴:\n${bidLog}`
+        );
+
+        //落札者DM
+        robot.messageRoom(
+          auctionWinnerId,
+          `[${auctionitemId}] *${description}* をあなたが *${finishPrice}N* コインで落札しました！:tada:\n出品者と連絡を取りましょう。`
+        );
+      } else {
+        //入札者無し 出品者DM
+        robot.messageRoom(
+          userId,
+          `オークションに出品していた [${auctionitemId}] *${description}* の入札期限日になりましたが、入札者は0人でした...:cry:。またのご利用をお待ちしております。`
+        );
+      }
+      return AuctionItem.destroy({ where: { auctionitemId: auctionitemId } });
+    })
+    .then(auctionItem => {
+      //オークションアイテム削除時
+    })
+    .catch(e => {
+      switch (e) {
+        case 'auction item not found':
+          console.log('not found');
+          //対象商品が見つからない
+          break;
+        case 'permission denied':
+          //本人以外が終了操作
+          console.log('permission denied');
+          break;
+        default:
+          robot.logger.error(e);
+          break;
+      }
+    });
+}
+
+/**
+ * オークションに出品
+ * @param {robot} robot apiのrobotオブジェクト
+ * @param {msg} msg apiのmsgオブジェクト
+ * @param {string} userId 出品者のuser ID
+ * @param {int} timeLimit 受付期間(日単位/30日以下)
+ * @param {int} startPrice 初期価格
+ * @param {string} description 商品の説明
+ */
 function createAuctionItem(
   robot,
   msg,
@@ -331,7 +371,7 @@ function createAuctionItem(
         .replace('<userId>', userId)
         .replace('<startPrice>', startPrice)
         .replace('<timeLimit>', timeLimit)
-        .replace('<id>', auctionItem.dataValues.id)
+        .replace('<id>', auctionItem.dataValues.auctionitemId)
         .replace(
           '<date>',
           moment(auctionItem.dataValues.createdAt).format(dateFormat)
@@ -340,7 +380,14 @@ function createAuctionItem(
   });
 }
 
-//入札
+/**
+ * 対象商品へ入札
+ * @param {robot} robot apiのrobotオブジェクト
+ * @param {msg} msg apiのmsgオブジェクト
+ * @param {int} auctionitemId 入札対象商品のオークションID
+ * @param {string} userId 入札者のユーザーID
+ * @param {int} bidAmount 入札額
+ */
 function bidAuctionItem(robot, msg, auctionitemId, userId, bidAmount) {
   if (bidAmount >= maxPrice) {
     msg.send(`*${maxPrice}N* コイン以上は扱えません。`);
@@ -355,7 +402,7 @@ function bidAuctionItem(robot, msg, auctionitemId, userId, bidAmount) {
   let description;
   let elapsedDay;
   let timeLimit;
-  AuctionItem.find({ where: { id: auctionitemId } })
+  AuctionItem.find({ where: { auctionitemId: auctionitemId } })
     .then(auctionItem => {
       if (auctionItem) {
         elapsedDay =
